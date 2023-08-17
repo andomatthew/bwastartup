e7d0be80ef5160d372a18ec881800a5a6f80178a
Alexandro M <alexandro.matthew@gmail.com>
2023-08-17 12:08:12 +0700

    email check api

--- handler/user.go
+++ handler/user.go
@@ -87,3 +87,45 @@ func (h *userHandler) Login(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) CheckEmailAvailability(c *gin.Context) {
+
+	var input user.CheckEmailInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormaValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.ApiResponse("Email checking failed", http.StatusUnprocessableEntity, "false", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	isEmailAvailable, err := h.userService.IsEmailAvailable(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": "Server error"}
+		response := helper.ApiResponse("Email checking failed", http.StatusUnprocessableEntity, "false", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	data := gin.H{
+		"is_available": isEmailAvailable,
+	}
+
+	var metaMessage string
+
+	if isEmailAvailable {
+		metaMessage = "Email is available"
+	} else {
+		metaMessage = "Email is NOT available"
+	}
+
+	response := helper.ApiResponse(metaMessage, http.StatusOK, "success", data)
+		c.JSON(http.StatusOK, response)
+
+
+}
+
--- user/service.go
+++ user/service.go
@@ -9,6 +9,7 @@ import (
 type Service interface {
 	RegisterUser(input RegisterUserInput) (User, error)
 	LoginUser(input LoginUserInput) (User, error)
+	IsEmailAvailable(input CheckEmailInput) (bool, error)
 }
 
 // membuat struct service dari interface Repository,
@@ -65,4 +66,21 @@ func (s *service) LoginUser(input LoginUserInput) (User, error) {
 	}
 
 	return user, nil
+}
+
+func (s *service) IsEmailAvailable(input CheckEmailInput) (bool, error) {
+	email := input.Email
+
+	user, err := s.repository.FindByEmail(email)
+
+	if err != nil {
+		return false, err
+	}
+
+	if user.ID == 0 {
+		return true, nil
+	}
+
+	return false, nil
+
 }
